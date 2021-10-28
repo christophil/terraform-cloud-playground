86a74fa1f635867c879f5a35651dd9f252cea58c
Christopher <39086671+christophil@users.noreply.github.com>
2021-10-28 16:24:12 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,6 @@ provider "aws" {
 resource "aws_instance" "myec2" {
   ami           = "ami-0057d8e6fb0692b80"
   instance_type = "t2.micro"
-  key_name      = "terraform-instance"
   tags = {
     "Name" = "ec2-terraform"
   }
